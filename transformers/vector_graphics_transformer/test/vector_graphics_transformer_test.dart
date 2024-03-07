73d933391ed05020d92a9f9647fc18db9e0786f1
Andrew Kolos <andrewrkolos@gmail.com>
2024-03-07 14:29:58 -0800

    use vector_graphics_compiler instead of local path dependency

--- transformers/vector_graphics_transformer/test/vector_graphics_transformer_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:vector_graphics_transformer/vector_graphics_transformer.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
